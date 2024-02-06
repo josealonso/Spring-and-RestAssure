17b36774f3f164ad7c5e9a75e6b3f5c84d837a94
josealonso <jose.alonso.programmer@gmail.com>
2024-02-06 13:34:23 +0100

    Using RestAssuredMockMvc with Spring’s test tools for our integration tests.

--- src/test/java/info/josealonso/usingRestAssured/DemoApplicationTests.java
+++ src/test/java/info/josealonso/usingRestAssured/DemoApplicationTests.java
@@ -2,20 +2,36 @@ package info.josealonso.usingRestAssured;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class DemoApplicationTests {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
 	@BeforeAll
-	public static void initialiseRestAssuredMockMvcStandalone() {
-//		RestAssuredMockMvc.webAppContextSetup;
-//		RestAssuredMockMvc.standaloneSetup(new TodosController());
+	public void initialiseMockMvcApplicationContext() {
+		RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+	}
+
+	@Test
+	public void givenNoMatchingCourseCodeWhenGetCourseThenRespondWithStatusNotFound() {
+		String nonMatchingCourseCode = "nonMatchingCourseCode";
+
+		given()
+				.when()
+				.get("/courses/" + nonMatchingCourseCode)
+				.then()
+				.log().ifValidationFails()
+				.statusCode(NOT_FOUND.value());
 	}
 
 }
