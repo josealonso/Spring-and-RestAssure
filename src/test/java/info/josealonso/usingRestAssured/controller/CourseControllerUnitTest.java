21c9bd0d7d0914ff0041f1a8cbc7c714b1db4569
josealonso <jose.alonso.programmer@gmail.com>
2024-02-06 13:03:17 +0100

    Testing the /courses endpoint.

--- src/test/java/info/josealonso/usingRestAssured/controller/CourseControllerUnitTest.java
+++ src/test/java/info/josealonso/usingRestAssured/controller/CourseControllerUnitTest.java
@@ -1,10 +1,22 @@
 package info.josealonso.usingRestAssured.controller;
 
 import info.josealonso.usingRestAssured.service.CourseService;
+import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import java.util.Collections;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.OK;
+
+//import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
 
 public class CourseControllerUnitTest {
 
@@ -16,7 +28,21 @@ public class CourseControllerUnitTest {
     private CourseControllerExceptionHandler courseControllerExceptionHandler;
 
     @BeforeAll
-    public static void initialiseRestAssuredMockMvcStandalone() {
+    public void initialiseRestAssuredMockMvcStandalone() {
         RestAssuredMockMvc.standaloneSetup(courseController, courseControllerExceptionHandler);
+    }
+
+    @Test
+    public void givenNoExistingCoursesWhenGetCoursesThenResponseWithStatusOkAndEmptyArray() {
+        when(courseService.getCourses()).thenReturn(Collections.emptyList());
+
+        given()
+            .when()
+                .get("/courses")
+            .then()
+                .log().ifValidationFails()
+                .statusCode(OK.value())
+                .contentType(ContentType.JSON)
+                .body(is(equalTo("[]")));
     }
 }
